01f7a0945eb548b774a7970f401fe94e9474bb81
Juan Ra <up230190@alumnos.upa.edu.mx>
2026-02-21 01:25:07 -0600

    Ejercicos h5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,51 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map
+        (\grade ->
+            if grade >= 7 then
+                Approved
+
+            else if grade >= 0 then
+                Failed
+
+            else
+                Pending
+        )
+        grades
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction list =
+    List.map airplaneScheduleAction list
